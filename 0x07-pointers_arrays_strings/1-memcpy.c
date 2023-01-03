a5c6fef526cdb0baf56ed285219dcbe723a636d6
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-01-03 15:23:11 -0800

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,35 +1,22 @@
 #include "main.h"
 
 /**
- *	_memcpy - a function that copies memory area
- * 	@dest: memory where is stored
- *    	@src: memory where is copied
- *     	@n: number of bytes
- *   	*Return: copied memory with n byted changed
- */
+  * _memcpy - irontnoux2
+  *
+  * @dest: pointer to char params
+  * @src: pointer to char params
+  * @n: size
+  *
+  * Return: *dest
+  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
+	unsigned int i;
 
-		int r = 0;
-
-			int i = n;
-
-
-
-				for (; r < i; r++)
-
-						{
-
-									dest[r] = src[r];
-
-											n--;
-
-												}
-
-					return (dest);
-
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
-
-
