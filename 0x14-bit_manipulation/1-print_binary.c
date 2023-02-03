f39bd88240ec6d60159df44a9cdb73910f60f2d6
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-02-03 13:13:28 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
@@ -21,5 +22,5 @@ void print_binary(unsigned long int n)
 			_putchar('0');
 	}
 	if (!count)
-
-		
+		_putchar('0');
+}
